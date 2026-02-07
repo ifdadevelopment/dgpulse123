c8f66cea78b3273e3fd332f88b7130dc7bb363df
ifdadevelopment <ifdadevelopment.team@gmail.com>
2026-02-07 16:59:43 +0530

    addNew

--- src/app/(site)/components/InnerHeroSection.jsx
+++ src/app/(site)/components/InnerHeroSection.jsx
@@ -51,7 +51,6 @@ export default function InnerHeroSection({ slug }) {
                                 </button>
                             </div>
                         )}
-
                     </div>
                 </div>
             </div>
